34215ebc1bc887b5a78afa6c8b33abd8a574352e
hevul <vuladac.rub@gmail.com>
2025-03-10 10:18:42 +0300

    Update login page (add logic)

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,26 @@
 import styles from "./Login.module.css";
 import image from "../../humans.svg";
+import { useState } from "react";
+import { useAuth } from "../../contexts/AuthContext";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState<string | null>(null);
+
+  const { login } = useAuth();
+
+  const handleLogin = async () => {
+    setError("");
+
+    try {
+      await login(username, password);
+    } catch (err: any) {
+      if (err.response) console.log(err.response);
+      setError("Invalid username or password");
+    }
+  };
+
   return (
     <div className={styles.page}>
       <div className={styles.form}>
@@ -10,13 +29,23 @@ const Login = () => {
           <h1>Войдите в аккаунт</h1>
           <div>
             <p>Логин</p>
-            <input type="text" placeholder="Введите Ваш логин" />
+            <input
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              type="text"
+              placeholder="Введите Ваш логин"
+            />
           </div>
           <div style={{ marginTop: 38, marginBottom: 79 }}>
             <p>Пароль</p>
-            <input type="text" placeholder="Введите Ваш пароль" />
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="text"
+              placeholder="Введите Ваш пароль"
+            />
           </div>
-          <button>Войти</button>
+          <button onClick={handleLogin}>Войти</button>
           <div className={styles.register}>
             <p>Ещё не создали аккаунт?</p>
             <a href="/register">Создать аккаунт</a>
