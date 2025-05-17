f47fb30c0df957e13e2860c3bd0d29d56353b659
hevul <vuladac.rub@gmail.com>
2025-05-17 14:53:24 +0300

    Убрал контекстное меню для кнопки хранилища.

--- src/pages/Dashboard/components/StorageButton/StorageButton.tsx
+++ src/pages/Dashboard/components/StorageButton/StorageButton.tsx
@@ -1,14 +1,6 @@
 import { FC } from "react";
 import styles from "./StorageButton.module.css";
 import { useStorage } from "../../../../contexts/StorageContext";
-import ContextMenuArea from "../../../../components/ContextMenuArea/ContextMenuArea";
-import EditIcon from "../../../../components/icons/EditIcon";
-import DeleteIcon from "../../../../components/icons/DeleteIcon";
-import PlusIcon from "../../../../components/icons/PlusIcon";
-import CreateStorageDialog from "../../../../components/dialogs/CreateStorageDialog/CreateStorageDialog";
-import { useDialog } from "../../../../contexts/DialogContext";
-import RenameStorageDialog from "../../../../components/dialogs/RenameStorageDialog/RenameStorageDialog";
-import DeleteStorageDialog from "../../../../components/dialogs/DeleteStorageDialog/DeleteStorageDialog";
 import { useEntities } from "../../../../contexts/EntitiesContext";
 
 interface Props {
@@ -18,46 +10,10 @@ interface Props {
 }
 
 const StorageButton: FC<Props> = ({ name, id, className }) => {
-  const { storage, selectStorage, refresh } = useStorage();
-  const { open } = useDialog();
+  const { storage, selectStorage } = useStorage();
   const { setCurrentDir } = useEntities();
 
   const isActive = storage?.id === id;
-
-  const items = [
-    {
-      title: "Создать хранилище",
-      icon: <PlusIcon />,
-      action: () => open(<CreateStorageDialog onSuccess={refresh} />),
-      hasSeparator: true,
-    },
-    {
-      title: `Переименовать хранилище`,
-      icon: <EditIcon width="18" />,
-      action: () => {
-        if (storage)
-          open(
-            <RenameStorageDialog storage={{ id, name }} onSuccess={refresh} />
-          );
-      },
-    },
-    {
-      title: `Удалить хранилище`,
-      icon: <DeleteIcon width="18" />,
-      action: () => {
-        if (storage)
-          open(
-            <DeleteStorageDialog
-              storage={{ id, name }}
-              onSuccess={() => {
-                refresh();
-                selectStorage(null);
-              }}
-            />
-          );
-      },
-    },
-  ];
 
   const handleClick = () => {
     selectStorage({ id, name });
@@ -65,18 +21,16 @@ const StorageButton: FC<Props> = ({ name, id, className }) => {
   };
 
   return (
-    <ContextMenuArea items={items}>
-      <div className={styles.div}>
-        <button
-          style={{ color: isActive ? "#4676FB" : "#ADC0F8" }}
-          className={styles.button}
-          onClick={handleClick}
-        >
-          {name}
-        </button>
-        {isActive ? <div className={styles.underline}></div> : null}
-      </div>
-    </ContextMenuArea>
+    <div className={styles.div}>
+      <button
+        style={{ color: isActive ? "#4676FB" : "#ADC0F8" }}
+        className={styles.button}
+        onClick={handleClick}
+      >
+        {name}
+      </button>
+      {isActive ? <div className={styles.underline}></div> : null}
+    </div>
   );
 };
 
